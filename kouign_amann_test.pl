d567912562f3ae2ee9329629e04db2a51f135279
gene <gene@makale.molprof.net>
2017-09-14 17:30:25 +0900

    only max fee

--- kouign_amann_test.pl
+++ kouign_amann_test.pl
@@ -26,8 +26,6 @@ for(my $i=1;$i<=16;$i++) {
 		my $limit_days=0;
 		my $limit_days_payments=0;
 
-#		print Dumper(@combo)."\n";
-
 		foreach my $combo(@combo) {
 			my ($pay,$day)=(split("\,",$combo))[0,1];
 			$limit_days+=$day;
@@ -35,15 +33,18 @@ for(my $i=1;$i<=16;$i++) {
 			$limit_days_payments+=$pay;
 			
 			$limit_days_payments{$limit_days_payments}=$limit_days;
-#			push(@payments,$limit_days_payments);
-			print "$limit_days\t".$combo."\n";
+#			print "$limit_days\t".$combo."\n";
 #			print "$limit_days\t".join("\t",@combo)."\n";
 		}
-		print "\n";
+#		print "\n";
 	}
 
 }
-print "=========== max payments ============\n";
-foreach my $pay(sort {$b <=> $a} keys %limit_days_payments) {
-	print $limit_days_payments{$pay}."\t".$pay."\n";
-}
+
+my @payments_by_pay=(sort {$b <=> $a} keys %limit_days_payments);
+print $payments_by_pay[0]."\n";
+
+#print "=========== max payments ============\n";
+#foreach my $pay(sort {$b <=> $a} keys %limit_days_payments) {
+#	print $limit_days_payments{$pay}."\t".$pay."\n";
+#}
